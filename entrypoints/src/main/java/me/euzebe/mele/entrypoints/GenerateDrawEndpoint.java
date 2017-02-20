b038daa22ce54303a7915f5ecf1deec81f4d245a
Jean Martineau-Figuette <jean.martineau-figuette@lyra-network.com>
2017-02-20 13:42:19 +0100

    add controller mapping

--- entrypoints/src/main/java/me/euzebe/mele/entrypoints/GenerateDrawEndpoint.java
+++ entrypoints/src/main/java/me/euzebe/mele/entrypoints/GenerateDrawEndpoint.java
@@ -1,9 +1,11 @@
 package me.euzebe.mele.entrypoints;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class GenerateDrawEndpoint {
 
 	@GetMapping("/")
