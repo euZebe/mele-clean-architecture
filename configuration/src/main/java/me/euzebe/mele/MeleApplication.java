2049eb21d548c2bc992be7e07770b714b10975c6
eeesebio@macPro <eeesebio@gmail.com>
2017-02-24 17:59:08 +0100

    fixing previous commit

--- configuration/src/main/java/me/euzebe/mele/MeleApplication.java
+++ configuration/src/main/java/me/euzebe/mele/MeleApplication.java
@@ -1,14 +1,13 @@
 package me.euzebe.mele;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import me.euzebe.mele.infrastructure.DrawMemoryRepository;
 import me.euzebe.mele.spi.DrawsCatalog;
 import me.euzebe.mele.usecase.generatedraw.GenerateDraw;
 import me.euzebe.mele.usecase.generatedraw.GenerateDrawController;
-import me.euzebe.mele.usecase.generatedraw.JsonMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeleApplication {
@@ -20,11 +19,6 @@ public class MeleApplication {
 	@Bean
 	GenerateDraw getGenerateDrawBean() {
 	    return new GenerateDrawController(getDrawsCatalog());
-	}
-
-	@Bean
-	JsonMapper getJsonMapper() {
-	    return new JsonMapper();
 	}
 
 	@Bean
--- entrypoints/src/main/java/me/euzebe/mele/entrypoints/GenerateDrawEndpoint.java
+++ entrypoints/src/main/java/me/euzebe/mele/entrypoints/GenerateDrawEndpoint.java
@@ -1,18 +1,16 @@
 package me.euzebe.mele.entrypoints;
 
+import javaslang.control.Option;
+import me.euzebe.mele.usecase.generatedraw.Draw;
+import me.euzebe.mele.usecase.generatedraw.DrawWithRandom;
+import me.euzebe.mele.usecase.generatedraw.GenerateDraw;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javaslang.control.Option;
-import me.euzebe.mele.usecase.generatedraw.Draw;
-import me.euzebe.mele.usecase.generatedraw.DrawWithRandom;
-import me.euzebe.mele.usecase.generatedraw.GenerateDraw;
-import me.euzebe.mele.usecase.generatedraw.JsonMapper;
 
 @RestController
 @RequestMapping("/api")
@@ -21,8 +19,6 @@ public class GenerateDrawEndpoint {
     @Autowired
     private GenerateDraw generateDrawController;
 
-    @Autowired
-    private JsonMapper mapper;
 
 	@Autowired
 	private DrawMapper drawMapper;
@@ -30,14 +26,6 @@ public class GenerateDrawEndpoint {
 	@GetMapping("/")
 	public String sayHi() {
 		return "Hi all!";
-	}
-
-	// localhost:7777/api/generateDraw?participants=jean,eusebe,ezechiel
-	@GetMapping(path = "/generateDraw", produces = "application/json")
-	public String generateDraw(@RequestParam("participants") String participants) {
-        Option<Draw> draw = generateDrawController.generateDraw(participants.split(","));
-
-		return mapper.toJson(draw.get());
 	}
 
 	@PostMapping(path = "/generateDraw", produces = "application/json")
